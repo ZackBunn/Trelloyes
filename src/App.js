563fb7d2454f0cc6e84f2372d2a137587ddbe94f
Zack Bunn <zrb6578@gmail.com>
2019-03-09 20:48:15 -0600

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,62 @@
 import React, { Component } from 'react';
 import List from './List';
 import './App.css';
+import STORE from './STORE'
+
+const newRandomCard = () => {
+  const id = Math.random().toString(36).substring(2, 4)
+    + Math.random().toString(36).substring(2, 4);
+  return {
+    id,
+    title: `Random Card ${id}`,
+    content: 'lorem ipsum',
+  }
+}
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
-    }
+  state = {
+    store: STORE
   };
 
+  handleDelete = (cardId) => {
+    const {lists, allCards} = this.state.store;
+
+    const newList = list.map(list => {
+      list.cardIds = list.cardIds.filter(id => id !== cardId);
+      return list;
+    });
+    delete allCards[cardId];
+
+    this.setState({
+      store: {
+        lists: newList, allCards
+      }
+    })
+  }
+
+  handleAdd = (listId) => {
+    const newCard = newRandomCard()
+
+    const newList = this.state.store.lists.map(list => {
+      if (list.id === listId)  {
+        list.cardIds.push(newCard.id)
+      }
+      return list
+    })
+  
+  this.setState ({
+    store: {
+      lists: newList, allCards: {
+        ...this.state.store.allCards, 
+        [newCard.id]: newCard
+      }
+    }
+  })  
+  }
+
+
   render() {
-    const { store } = this.props
+    const { store } = this.state
     return (
       <main className='App'>
         <header className='App-header'>
@@ -21,9 +66,11 @@ class App extends Component {
           {store.lists.map(list => (
             <List
               key={list.id}
+              id={list.id}
               header={list.header}
               cards={list.cardIds.map(id => store.allCards[id])}
-            />
+              onClickDelete={this.handleDeleteCard}
+              onClickAdd={this.handleAddCard} />
           ))}
         </div>
       </main>
--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Card.css';
 
+Card.propTypes = {
+    onClickDelete: () => {}
+}
 export default function Card(props) {
     return (
     <div class="Card">
-        <button type="button">delete</button>
+        <button type="button" onClick={() => props.onClickDelete(props.id)}>delete</button>
         <h3>{props.title}</h3>
         <p>{props.content}</p>
     </div>
     );
 }
+
